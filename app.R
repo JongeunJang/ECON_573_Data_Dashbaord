8f1d8985d3fcd307b6cd8601d954e1cbb2868948
Jongeun Jang <ch0521k@gmail.com>
2026-03-22 15:40:22 -0700

    Initial commit

---
+++ app.R
@@ -0,0 +1,172 @@
+library(shiny)
+library(leaflet)
+library(dplyr)
+library(ggplot2)
+library(sf)
+library(lubridate)
+library(ggrepel)
+library(plotly)
+
+# Load pre-processed data
+preprocessed_data <- readRDS("./data/preprocessed_data.rds")
+data <- preprocessed_data$data
+us_counties <- preprocessed_data$us_counties
+
+# UI
+ui <- fluidPage(
+  titlePanel("Hurricane Strikes and Home Values Dashboard"),
+  sidebarLayout(
+    sidebarPanel(
+      h4("Counties with Hurricanes"),
+      # This will be populated from the server
+      selectInput("county_select", "Select a county:", choices = NULL, width = "100%"),
+      br(),
+      hr(),
+      h5("Data Sources:"),
+      tags$ul(
+        tags$li(tags$a(href = "https://www.fema.gov/openfema-data-page/disaster-declarations-summaries-v2", "FEMA Disaster Declarations Summaries")),
+        tags$li(tags$a(href = "https://www.zillow.com/research/data/", "Zillow Home Value Index (ZHVI)")),
+        tags$li(tags$a(href = "https://www.census.gov/geographies/mapping-files/time-series/geo/tiger-line-file.html", "US Counties Shapefile"))
+      )
+    ),
+    mainPanel(
+      h4("Select a County on the Map or From the List"),
+      p("Click on a highlighted county or select from the dropdown to view home value trends."),
+      HTML('<div>
+              <span style="background-color: yellow; border: 1px solid black; padding: 0px 10px;">&nbsp;</span> With Hurricane
+              &nbsp;&nbsp;
+              <span style="background-color: lightgray; border: 1px solid black; padding: 0px 10px;">&nbsp;</span> Without Hurricane
+            </div><br/>'),
+      leafletOutput("map"),
+      br(),
+      plotlyOutput("timeSeries"),
+      uiOutput("graphNote")
+    )
+  )
+)
+
+# Server
+server <- function(input, output, session) {
+
+  # Prepare and set the choices for the dropdown
+  hurricane_county_list <- data %>%
+    filter(hurricane_flag) %>%
+    distinct(fips, RegionName, StateName) %>%
+    mutate(fips = as.character(fips)) %>%
+    arrange(StateName, RegionName) %>%
+    mutate(display_name = paste0(RegionName, ", ", StateName))
+
+  county_choices <- setNames(hurricane_county_list$fips, hurricane_county_list$display_name)
+
+  updateSelectInput(session, "county_select", choices = c("Select a county" = "", county_choices))
+
+  # Reactive value for selected county
+  selected_county <- reactiveVal(NULL)
+
+  # Observer for map clicks on hurricane counties
+  observeEvent(input$map_shape_click, {
+    # Only react to clicks on counties that are in our hurricane list
+    req(input$map_shape_click$id %in% county_choices)
+    selected_county(input$map_shape_click$id)
+  })
+
+  # Observer for dropdown selection
+  observeEvent(input$county_select, {
+    # Ignore the initial blank value
+    req(input$county_select != "")
+    selected_county(input$county_select)
+  })
+
+  # When the selected county changes, update the dropdown menu to keep it in sync
+  observeEvent(selected_county(), {
+    req(selected_county())
+    updateSelectInput(session, "county_select", selected = selected_county())
+  })
+
+  # Map
+  output$map <- renderLeaflet({
+    leaflet(us_counties) %>%
+      addTiles() %>%
+      setView(lng = -98.57, lat = 39.82, zoom = 4) %>%
+      addPolygons(
+        fillColor = ~ifelse(!is.na(RegionName), "yellow", "lightgray"),
+        fillOpacity = 0.5,
+        color = "black",
+        weight = 1,
+        layerId = ~fips,
+        popup = ~paste(RegionName, StateName, sep = ", ")
+      )
+  })
+
+  # Time series plot
+  output$timeSeries <- renderPlotly({
+    req(selected_county())
+    county_data <- data %>% filter(fips == selected_county())
+
+    if (nrow(county_data) == 0) return(NULL)
+
+    # Get hurricane periods
+    hurricanes <- county_data %>%
+      filter(hurricane_flag) %>%
+      mutate(end_date = date %m+% years(1),
+             ay_offset = -40)
+
+    # Simple algorithm to repel overlapping labels
+    if(nrow(hurricanes) > 1) {
+      hurricanes <- hurricanes %>% arrange(date)
+      for(i in 2:nrow(hurricanes)) {
+        if(difftime(hurricanes$date[i], hurricanes$date[i-1], units = "days") < 365 * 2) { # If hurricanes are within 2 years
+          # Offset the ay parameter (text position) instead of the arrow tip (zhvi_value)
+          hurricanes$ay_offset[i] <- hurricanes$ay_offset[i-1] - 30
+        }
+      }
+    }
+
+    plot_ly(county_data, x = ~date, y = ~zhvi_value, type = 'scatter', mode = 'lines', name = "ZHVI",
+            text = ~paste("Month:", format(date, "%Y-%m"),
+                          "<br>ZHVI:", format(round(zhvi_value, 0), big.mark = ","),
+                          ifelse(!is.na(hurricane_names), paste("<br>Hurricane:", hurricane_names), "<br>Hurricane: -")),
+            hoverinfo = 'text') %>%
+      layout(
+        title = paste("Monthly Home Values in", unique(county_data$RegionName), unique(county_data$StateName)),
+        xaxis = list(title = "Date"),
+        yaxis = list(title = "ZHVI", tickformat = ","),
+        showlegend = FALSE,
+        shapes = lapply(1:nrow(hurricanes), function(i) {
+          list(
+            type = "rect",
+            fillcolor = "yellow",
+            line = list(width = 0),
+            opacity = 0.2,
+            x0 = hurricanes$date[i],
+            x1 = hurricanes$end_date[i],
+            y0 = 0,
+            y1 = 1,
+            yref = "paper",
+            layer = "below",
+            hoverinfo = "none"
+          )
+        }),
+        annotations = lapply(1:nrow(hurricanes), function(i) {
+          list(
+            x = hurricanes$date[i],
+            y = hurricanes$zhvi_value[i],
+            text = hurricanes$hurricane_names[i],
+            showarrow = TRUE,
+            arrowhead = 4,
+            standoff = 0,
+            ax = 0,
+            ay = hurricanes$ay_offset[i]
+          )
+        })
+      )
+  })
+
+  output$graphNote <- renderUI({
+    req(selected_county())
+    p(HTML("<b>Note:</b> The yellow shaded area in the graph represents the 1-year period following a hurricane strike."))
+  })
+}
+
+# Run the app
+print(shinyApp(ui = ui, server = server))
---
+++ data/_prepare_data.R
@@ -0,0 +1,102 @@
+# Data Sources:
+# 1. FEMA Disaster Declarations Summaries: https://www.fema.gov/openfema-data-page/disaster-declarations-summaries-v2
+# 2. Zillow Home Value Index (ZHVI) - County, All Homes, Smoothed, Seasonally Adjusted: https://www.zillow.com/research/data/
+# 3. US Counties Shapefile: https://www.census.gov/geographies/mapping-files/time-series/geo/tiger-line-file.html
+
+# Setup
+library(dplyr)
+library(tidyr)
+library(readr)
+library(stringr)
+library(lubridate)
+library(sf)
+library(tigris)
+library(rmapshaper)
+
+# Load raw data
+fema_raw <- read_csv("./data/DisasterDeclarationsSummaries_raw.csv")
+zillow_raw <- read_csv("./data/County_zhvi_uc_sfrcondo_tier_0.33_0.67_sm_sa_month_raw.csv")
+us_counties_shapefile <- readRDS("./data/us_counties_2020.rds")
+
+# Process FEMA data
+fema_clean <- fema_raw %>%
+  filter(incidentType == "Hurricane", declarationType == "DR") %>%
+  mutate(
+    fipsStateCode = str_pad(fipsStateCode, width = 2, pad = "0"),
+    fipsCountyCode = str_pad(fipsCountyCode, width = 3, pad = "0"),
+    fips = paste0(fipsStateCode, fipsCountyCode),
+    incident_date = as.Date(incidentBeginDate)
+  ) %>%
+  select(fips, femaDeclarationString, incident_date, hurricane_name = declarationTitle)
+
+fema_monthly <- fema_clean %>%
+  mutate(incident_month = floor_date(incident_date, "month")) %>%
+  group_by(fips, incident_month) %>%
+  summarize(
+    hurricane_names = str_c(unique(hurricane_name), collapse = "; "),
+    femaDeclarationStrings = str_c(unique(femaDeclarationString), collapse = "; "),
+    n_hurricanes = n(),
+    .groups = "drop"
+  )
+
+# Process Zillow data
+zillow_clean <- zillow_raw %>%
+  pivot_longer(
+    cols = matches("^19|^20"),
+    names_to = "date",
+    values_to = "zhvi_value"
+  ) %>%
+  drop_na(zhvi_value) %>%
+  mutate(
+    StateCodeFIPS = str_pad(StateCodeFIPS, width = 2, pad = "0"),
+    MunicipalCodeFIPS = str_pad(MunicipalCodeFIPS, width = 3, pad = "0"),
+    fips = paste0(StateCodeFIPS, MunicipalCodeFIPS),
+    date = ymd(date)
+  ) %>%
+  select(fips, RegionName, StateName, date, zhvi_value)
+
+# Combine Zillow and FEMA data
+data_hurricane_housing <- zillow_clean %>%
+  mutate(date = floor_date(date, "month")) %>%
+  left_join(fema_monthly, by = c("fips" = "fips", "date" = "incident_month")) %>%
+  mutate(
+    hurricane_flag = if_else(!is.na(hurricane_names), TRUE, FALSE)
+  ) %>%
+  select(fips, date, RegionName, StateName, zhvi_value, hurricane_flag, n_hurricanes, hurricane_names, femaDeclarationStrings)
+
+
+# Prepare data for map: counties with hurricanes
+counties_with_hurricanes <- data_hurricane_housing %>%
+  filter(hurricane_flag) %>%
+  distinct(fips, RegionName, StateName) %>%
+  mutate(fips = as.character(fips))
+
+# Merge with shapefile
+
+continental_state_fips <- c(
+  "01", "04", "05", "06", "08", "09", "10", "12", "13", "16", "17", "18", "19", "20",
+  "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34",
+  "35", "36", "37", "38", "39", "40", "41", "42", "44", "45", "46", "47", "48", "49",
+  "50", "51", "53", "54", "55", "56"
+)
+
+us_counties_simplified <- us_counties_shapefile %>%
+  filter(substr(GEOID, 1, 2) %in% continental_state_fips) %>%
+  ms_simplify(keep = 0.05, keep_shapes = TRUE)
+
+
+# Merge with hurricane data
+us_counties_processed <- us_counties_simplified %>%
+  mutate(fips = GEOID) %>%
+  left_join(counties_with_hurricanes, by = "fips")
+
+# Save the processed data objects to an RDS file
+saveRDS(
+  list(
+    data = data_hurricane_housing,
+    us_counties = us_counties_processed
+  ),
+  file = "./data/preprocessed_data.rds"
+)
+
+print("Data preprocessing complete. 'preprocessed_data.rds' saved to data directory.")
